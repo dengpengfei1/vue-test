38b2f2d7a67273f27d5ddb8052710bf2fb7ba5ce
dengpengfei <dengpengfei@360.net>
2018-02-28 19:46:06 +0800

    vuex

--- src/components/itemContainer.vue
+++ src/components/itemContainer.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <header>
+    <header class="test_tip">
+      <!-- <span v-if="itemType === 'home'">{{active_topic_phase}}</span> -->
+      <!-- <span v-else-if="itemType === 'item'">{{topic_name}}</span> -->
     </header>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  props: ['itemType'],
+  computed: {
+    ...mapState(['itemDetail'])
+  },
+  created () {
+    console.log(this.itemDetail);
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Store from './store/'
 import './config/rem'
 import './style/common.less'
 
@@ -12,6 +13,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  Store,
   components: { App },
   template: '<App/>'
 })
